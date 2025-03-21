040db5a81bb557bd0583d8f2ce835a461d5315f4
zahid <zahidthoker5@gmail.com>
2025-03-22 00:46:44 +0530

    completed the timer app

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ const startbtn = document.getElementById("start");
 const timerDisplay = document.getElementById("timerdisplay");
 const stopbtn = document.getElementById("stop")
 const pausebtn = document.getElementById("pause")
-
+let flag = false;
+let remainingTime=0;
+let  interval;
 startbtn.addEventListener("click",startTimer);
 
 
@@ -15,33 +17,58 @@ function startTimer(){
         return;
     }
 
-    let interval = setInterval((event) => {
+            if(flag){
+                time = remainingTime;
+                flag=false;
+            }
+
+        interval = setInterval(() => {
         time--;
         timerDisplay.innerText = `${time} seconds remaining`;
         if(time<0){
-            timerDisplay.innerText = "timer complete"
+            timerDisplay.innerText = "Time is up!"
             input.value=""
             clearInterval(interval)
         }
         
         
-        pausebtn.addEventListener("click",pause)  
+ 
     }, 1000);
-          
+
+    
+
+
+    pausebtn.addEventListener("click",()=>{
+       if(!flag){
+            clearInterval(interval);
+            remainingTime=time;
+            timerDisplay.innerText =   `Paused at ${remainingTime} seconds`
+            flag=true;
+            pausebtn.innerText = "▶️Resume"
+        }else{
+            startTimer();
+            pausebtn.innerText = "⏸️Pause"
+        }
+    })
+       
     
 }
+
+
+stopbtn.addEventListener("click",()=>{
+    clearInterval(interval)
+    input.value=""
+    timerDisplay.innerText = ""
+    
+})
+
+
+
 
 function display(string){
     timerDisplay.innerText = string;
     input.value=" ";
     timerDisplay.focus;
     return;
-
-}
-stopbtn.addEventListener("click",stop)
-function stop(interval){
-    clearInterval(interval)
-    input.value=""
-    timerDisplay.innerText = ""
 
 }
